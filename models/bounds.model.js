3bc07f117bcd35c29043e41a1c638c69e92c46c4
leonh <hello@leonh.fr>
2018-10-22 18:43:46 +0200

    style: bounds instructions

--- models/bounds.model.js
+++ models/bounds.model.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports.getBoundaries = async () => {
-  // TODO: remove sleep, only used to simulate workload and show off my cool loader
-  await sleep(1000);
+  // I sleep to simulate workload and show off my cool loader
+  // For future devs: the pacman loader is a sacred part of the app
+  await sleep(600);
   return [
     process.env.GEO_BBOX_LEFT, process.env.GEO_BBOX_BOTTOM,
     process.env.GEO_BBOX_RIGHT, process.env.GEO_BBOX_TOP
